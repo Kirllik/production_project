20bfc375f4e538e186b24240d81a34d797b2d834
Kirllik <krejg007@rambler.ru>
2023-02-05 15:53:29 +0300

    6. Роутинг Code splitting Lazy Suspence без искусственной задержки

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -5,5 +5,6 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
         port: options.port,  //port
         open: true,          //Автоматическое открытия окна в браузере при запуске сервера
+        historyApiFallback: true,  //Для проксирования запросов через корневую страницу index.html, для SPA(одностраничников)
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Routes, Route} from 'react-router-dom';
 import {Counter} from "./component/Counter";
 import './component/index.scss'
+import {AboutPageAsync} from "./component/AboutPage/AboutPageAsync";
+import {MainPageAsync} from "./component/MainPage/MainPageAsync";
+import {Link} from 'react-router-dom';
 
 const App = () => {
     return (
         <div className="app">
+            <Link to={'/about'}>О нас</Link>
+            <Link to={'/'}>Главная</Link>
+            <Suspense fallback={<div>Загрузка...</div>}>  //Lazy_Loading
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
             Counter
             <Counter/>
         </div>
